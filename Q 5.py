39d008a7e202dd66c273a44f4ddb56edc0672785
Ahmed Mohamed <a.mohamed@alustudent.com>
2021-11-10 11:56:42 +0200

    5

--- Q 5.py
+++ Q 5.py
@@ -1,16 +1,22 @@
 x = [ 20, 40, 60, 80, 80]
-y = [ 2, 4]
-
+y = [ 3, 4]
+print("X = ", x)
+print("Y = ", y)
+Value = bool(True)
 
 for numy in y: # There is exists Y that ....
+    if Value == False:
+            break
     for numx in x: # for all of X ...
         if numx%numy == 0: #The condition
             Value = True
         else:
             Value = False # IF there is only one value of x that is false, the condition is false
             break
+        
+                    
 
 if Value == True:
-    print("The condition is True")
+    print("The condition Y|X is True")
 else: 
     print("The condition is False")
