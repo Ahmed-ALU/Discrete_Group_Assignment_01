7ab76568426461820ae7ac554c72e7f50940143c
Ahmed Mohamed <a.mohamed@alustudent.com>
2021-11-10 21:18:09 +0200

    You see, we like to stress ourselves

--- Q 5.py
+++ Q 5.py
@@ -1,18 +1,18 @@
 
 
 x_list = list([20, 30, 40, 50])
-y_list = list([5, 6, 11, 7])
+y_list = list([7, 6, 11, 5])
 result_list = list()
 
-x_len = int(input("Please enter the number of elements in the list [X]: "))
-for i in range(x_len):
-    inp = int(input("Please input the list element here: "))
-    x_list.append(inp)
+# x_len = int(input("Please enter the number of elements in the list [X]: "))
+# for i in range(x_len):
+#     inp = int(input("Please input the list element here: "))
+#     x_list.append(inp)
 
-y_len = int(input("Please enter the number of elements in the list [y]: "))
-for i in range(y_len):
-    inp = int(input("Please input the list element here: "))
-    y_list.append(inp)
+# y_len = int(input("Please enter the number of elements in the list [y]: "))
+# for i in range(y_len):
+#     inp = int(input("Please input the list element here: "))
+#     y_list.append(inp)
 
 print("X = ", x_list)
 print("Y = ", y_list)
@@ -26,7 +26,9 @@ for numy in y_list: # There is exists Y that ....
         if numx%numy == 0: #The condition
             Value = True #continue looping
         else:
-            Value = False # IF there is only one value of x that is false, the condition is false for that specific element of Y
+            Value = False # IF there is only one value.... 
+            #....of x that is false, the condition is false
+            #.... for that specific element of Y
             break # break and go to the next y if exist
     
     if Value == True:
